fe47823d3030c6fd7265dbb545130beb65b238d2
Vinicius Andrade <vini.fisica.unb@gmail.com>
2023-07-08 17:33:15 -0400

    requisite 03 done

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,29 @@
-# Req 3
+import csv
+from models.dish import Dish
+from models.ingredient import Ingredient
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+
+        with open(source_path, "r") as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            rows = list(csv_reader)
+            dish_dict = dict()
+
+            for element in rows:
+                dish_name = element["dish"]
+                if dish_name in dish_dict:
+                    dish_dict[dish_name].append(element)
+                else:
+                    dish_dict[dish_name] = [element]
+
+            for key, value in dish_dict.items():
+                dish = Dish(key, float(value[0]["price"]))
+                for item in value:
+                    ingredient = Ingredient(item["ingredient"])
+                    dish.add_ingredient_dependency(
+                        ingredient, int(item["recipe_amount"])
+                    )
+                self.dishes.add(dish)
