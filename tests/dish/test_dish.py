a0e2ff78624f068e0935827a90efecad59875e58
Vinicius Andrade <vini.fisica.unb@gmail.com>
2023-07-08 17:21:41 -0400

    requisite 02 done

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,25 @@
-from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.dish import Dish
+import pytest
 
 
-# Req 2
 def test_dish():
-    pass
+    dish_spaghetti = Dish("spaghetti", 1)
+    dish_bacon = Dish("bacon", 2)
+
+    assert dish_spaghetti.name == "spaghetti"
+    assert hash(dish_spaghetti) != hash(dish_bacon)
+    assert hash(dish_spaghetti) == hash(dish_spaghetti)
+    assert dish_spaghetti != dish_bacon
+    assert dish_spaghetti == dish_spaghetti
+    assert repr(dish_spaghetti) == "Dish('spaghetti', R$1.00)"
+
+    with pytest.raises(TypeError):
+        Dish("spaghetti", "spaghetti")
+
+    with pytest.raises(ValueError):
+        Dish("spaghetti", -7.00)
+
+    dish_spaghetti.add_ingredient_dependency("cheese", 1)
+    assert dish_spaghetti.recipe.get("cheese") == 1
+    assert dish_bacon.get_restrictions() == set()
+    assert dish_bacon.get_ingredients() == set()
