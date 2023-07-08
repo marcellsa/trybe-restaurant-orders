ed46a46cbfde46ce3c57ce9f0ff54478bd4405b3
Vinicius Andrade <vini.fisica.unb@gmail.com>
2023-07-08 17:03:11 -0400

    requisite 01 done

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,16 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient
 
 
-# Req 1
 def test_ingredient():
-    pass
+    pork_bacon = Ingredient("bacon")
+    beef_bacon = Ingredient("bacon")
+    egg = Ingredient("egg")
+
+    assert pork_bacon.__hash__() == beef_bacon.__hash__()
+    assert pork_bacon.__hash__() != egg.__hash__()
+    assert pork_bacon == pork_bacon
+    assert beef_bacon != egg
+    assert repr(pork_bacon) == "Ingredient('bacon')"
+    assert beef_bacon.name == "bacon"
+    invalid_instance = Ingredient("xablau")
+    assert invalid_instance.restrictions == set()
